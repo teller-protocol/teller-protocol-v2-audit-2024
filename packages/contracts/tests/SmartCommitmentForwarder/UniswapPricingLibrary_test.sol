faaa0f46fcfebac79126e974bb6a81aa83516de8
Admazzola <admazzola@gmail.com>
2024-11-18 17:18:16 -0500

    adding coverage for uniswap pricing lib

--- packages/contracts/tests/SmartCommitmentForwarder/UniswapPricingLibrary_test.sol
+++ packages/contracts/tests/SmartCommitmentForwarder/UniswapPricingLibrary_test.sol
@@ -43,8 +43,7 @@ contract UniswapPricingLibraryTest is Testable {
     }
 
    function test_getUniswapPriceRatioForPool_same_price() public {
-        //collateralTokenDecimals = 6;
-
+      
         bool zeroForOne = false; // ??
 
         mockUniswapPool.set_mockSqrtPriceX96(1 * 2**96);
@@ -63,9 +62,73 @@ contract UniswapPricingLibraryTest is Testable {
         uint256 priceRatio = UniswapPricingLibrary
             .getUniswapPriceRatioForPool(routeConfig);
 
-        console.log("price ratio");
-        console.logUint(priceRatio);
+        
+
+        assertEq(  1e18 , priceRatio  , "unexpected price ratio") ;
     }
+
+
+      function test_getUniswapPriceRatioForPool_different_price() public {
+      
+        bool zeroForOne = false; // ??
+
+        
+        //i think this means the ratio is 100:1
+        mockUniswapPool.set_mockSqrtPriceX96(10 * 2**96);
+
+
+        uint32 twapInterval = 0;
+
+        IUniswapPricingLibrary.PoolRouteConfig
+            memory routeConfig = IUniswapPricingLibrary.PoolRouteConfig({
+                pool: address(mockUniswapPool),
+                zeroForOne: zeroForOne,
+                twapInterval: twapInterval,
+                token0Decimals: 18,
+                token1Decimals: 18
+            });
+
+        uint256 priceRatio = UniswapPricingLibrary
+            .getUniswapPriceRatioForPool(routeConfig);
+
+        
+
+        assertEq(  1e16 , priceRatio  , "unexpected price ratio") ;
+    }
+
+
+
+  function test_getUniswapPriceRatioForPool_decimal_scenario_A() public {
+      
+        bool zeroForOne = false; // ??
+
+        uint256  principalTokenDecimals = 18;
+        uint256 collateralTokenDecimals = 6;
+
+          
+
+        mockUniswapPool.set_mockSqrtPriceX96(1 * 2**96);
+
+        uint32 twapInterval = 0;
+
+    //decimals dont affect raw ratios 
+        IUniswapPricingLibrary.PoolRouteConfig
+            memory routeConfig = IUniswapPricingLibrary.PoolRouteConfig({
+                pool: address(mockUniswapPool),
+                zeroForOne: zeroForOne,
+                twapInterval: twapInterval,
+                token0Decimals: collateralTokenDecimals,
+                token1Decimals: principalTokenDecimals
+            });
+
+        uint256 priceRatio = UniswapPricingLibrary
+            .getUniswapPriceRatioForPool(routeConfig);
+
+        
+
+        assertEq(  1e18 , priceRatio  , "unexpected price ratio") ;
+    }
+
 
 
     
