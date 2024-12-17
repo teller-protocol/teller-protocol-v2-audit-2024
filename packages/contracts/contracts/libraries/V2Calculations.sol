f6d8a8881053350df6a90cc25447a1d54ee647ae
andy <admazzola@gmail.com>
2024-12-17 16:58:18 -0500

    removing min

--- packages/contracts/contracts/libraries/V2Calculations.sol
+++ packages/contracts/contracts/libraries/V2Calculations.sol
@@ -127,8 +127,11 @@ library V2Calculations {
             // NOTE: the last cycle could have less than the calculated payment amount
 
             //the amount owed for the cycle should never exceed the current payment cycle amount  so we use min here 
-            uint256 owedAmountForCycle = Math.min(  ((_bid.terms.paymentCycleAmount  * owedTime)  ) /
-                    _paymentCycleDuration , _bid.terms.paymentCycleAmount+interest_ ) ;
+            //uint256 owedAmountForCycle = Math.min(  ((_bid.terms.paymentCycleAmount  * owedTime)  ) /
+            //        _paymentCycleDuration , _bid.terms.paymentCycleAmount+interest_ ) ;
+
+            uint256 owedAmountForCycle =  ((_bid.terms.paymentCycleAmount  * owedTime)  ) /  
+                    _paymentCycleDuration  ; 
 
             uint256 owedAmount = isLastPaymentCycle
                 ? owedPrincipal_ + interest_
